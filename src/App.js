cf1e1db05824ab301c7a96183d5ffd9a728b195c
Himanshu Shekhar <himanshu.3.shekhar@bt.com>
2020-02-05 10:40:49 +0530

    Added material-ui

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Button from '@material-ui/core/Button';
 import logo from './logo.svg';
 import './App.scss';
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Button variant="contained" color="primary"> Welcome</Button>
       </header>
       <footer>
       </footer>
